2855e8f842d9b71812f87630ab2eefce788b840f
Praveen S <praveenss2806@gmail.com>
2023-02-10 22:06:08 +0530

    updated mainActivity UI

--- app/src/main/java/com/example/ttscoretracker/MainActivity.kt
+++ app/src/main/java/com/example/ttscoretracker/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.ttscoretracker
 
 import android.content.Intent
-import android.os.Build.VERSION_CODES.P
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
